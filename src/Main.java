e1920444de8caaf38235af52af33ee44140c6043
TheBigL <lmohamed1@studentmail.nait.ca>
2019-05-04 14:23:59 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+
+    }
+
+    public static String translateWord(String input)
+    {
+        int position = -5;
+
+
+        for(int i = 0; i < input.length(); i++)
+        {
+            if(isVowel(input.charAt(i)))
+            {
+                position = i;
+                break;
+            }
+        }
+
+        if(position == 0)
+        {
+            return input + "way";
+        }
+
+    }
+
+    public static boolean isVowel(char letter)
+    {
+        Character.toLowerCase(letter);
+            if(letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u' || letter == 'y')
+            {
+                return true;
+            }
+
+            else
+                return false;
+    }
+}
